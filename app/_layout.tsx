0c615f9cb55ebeabfd6a7531244d37e25a523431
Elvis Lambou <cabrelelvis187@gmail.com>
2025-11-16 01:22:40 +0100

    animated login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
-import { Stack } from "expo-router";
-
+import {
+  Nunito_400Regular,
+  Nunito_700Bold,
+  Nunito_900Black,
+} from "@expo-google-fonts/nunito";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+const queryClient = new QueryClient();
 export default function RootLayout() {
-  return <Stack />;
+  const insets = useSafeAreaInsets();
+  let [fontLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold,
+    Nunito_900Black,
+  });
+  if (!fontLoaded) return null;
+  return (
+    <GestureHandlerRootView
+      style={{ flex: 1, paddingBottom: insets.bottom, backgroundColor: "#fff" }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <Slot />
+      </QueryClientProvider>
+    </GestureHandlerRootView>
+  );
 }
